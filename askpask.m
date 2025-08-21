024b80f75030ece9b8fa48b8fce79c5c32e62e71
Md Ariful Islam <154077378+arifulmist@users.noreply.github.com>
2025-08-21 12:53:42 +0600

    Add files via upload

---
+++ askpask.m
@@ -0,0 +1,51 @@
+clc;
+clear all;
+close all;
+%step 1-: msg signal load
+bitstream=load('inout.txt')
+
+%step 2-basic Parameters
+
+fc=input('input fc:');
+tb=1;% bit duration
+fs=100;
+t=0:1/fs:tb-(1/fs)
+a=5;
+%implementation
+ASK=[];
+PSK=[];
+FSK=[];
+
+for i=1:length(bitstream)
+  if bitstream(i)==1
+      ask=2*a*cos(2*pi*fc*t);
+      psk=a*cos(2*pi*fc*t);
+      fsk=a*cos(2*pi*3*fc*t);
+  else
+      ask=(0.5)*a*cos(2*pi*fc*t);
+      psk=a*cos(2*pi*fc*t+pi);
+      fsk=a*cos(2*pi*(0.5)*fc*t);
+    end
+    ASK=[ASK ask];
+    PSK=[PSK psk];
+    FSK=[FSK fsk];
+
+end
+
+
+subplot(3,1,1)
+
+plot(ASK, 'pr', 'MarkerSize', 5);
+title('ASK');
+grid on;
+subplot(3,1,2)
+plot(PSK,'r');
+title('PSK');
+grid on;
+subplot(3,1,3)
+plot(FSK,'g');
+title('FSK');
+grid on;
+
+
+
---
+++ lab4.m
@@ -0,0 +1,66 @@
+close all;
+clc ;
+clear all;
+
+fm=100;
+fs=10000;
+t=0:1/fs:1000/fs;
+
+%message signal
+x=5*sin(2*pi*fm.*t);
+plot(t,x );
+hold on;
+%sampling
+y=[0];
+xr=0;
+del=0.4;
+for i=1:length(x)-1
+   if xr(i)<=x(i)
+     d=1;
+     xr(i+1)=xr(i)+del;
+   else
+     d=0;
+     xr(i+1)=xr(i)-del;
+   end
+   y=[y d];
+ end
+
+ stairs(t,xr);% shoew statre
+ title('staircase  Approximated Signal');
+ hold off;
+
+ mse=sum((x-xr).^2)/length(x);%mean square error
+ disp(['Mean squared error(mse):',num2str(mse)]);
+ figure;
+
+ subplot(3,1,1);
+ plot(t,y);
+ title('Delta Modulated signal');
+
+
+ %demodukated signal
+ subplot(3,1,2);
+ y_demod=[0];
+ x_demod=0;
+ for i=2:length(y)
+   if y(i)==1
+     x_demod=x_demod+del;
+   else
+      x_demod=x_demod-del;
+   end
+   y_demod=[y_demod x_demod];
+ end
+ plot(t,y_demod);
+ title('delta demodulated signal before filtering');
+
+ %apply low pass filter
+
+ filterorder=20;
+ lopas=fir1(filterorder,fm/(fs/2),'low');
+ rectss=filter(lopas,1,y_demod);
+
+ subplot(3,1,3);
+ plot(t,rectss);
+ title('filtered demodulation signal');
+
+
